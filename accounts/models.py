758a79d9e84d2116023f94c903c34bed06849874
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-02-17 18:53:07 +0100

    create class profile in models.py + makemigrations: 0001_initial

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    cuisine_specialization = models.CharField(max_length=100)
+    phone_number = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Account: {self.name}"
