609c3f16ecb7e524322b74d880a51f3e7734659a
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-02-20 11:32:06 +0100

    create template folder for project + create base.html + edit chefs.html page + edit settings.py

--- home_taste/settings.py
+++ home_taste/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['http://127.0.0.1:8000/', '127.0.0.1', '.herokuapp.com']
 
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'home_taste.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
