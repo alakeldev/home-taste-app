92177a43a3d47a1cab2781e72fd292a890a6a3b7
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-02-20 21:53:37 +0100

    import slugify + add save fun inside  profile model to set value to slug if not set by user + edit the profile model and add slug field + set debug to false in settings.py + migrations file 0004

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -13,9 +14,16 @@ class Profile(models.Model):
     phone_number = models.CharField(max_length=30)
     created_on = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='profile', blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True)
+
 
     class Meta:
         ordering = ["-created_on"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.user.username)
+        super(Profile, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"User: {self.user} | Name: {self.name}"
