29caffae82599b4cb137e1b321eabf1bac137555
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-02-22 13:24:55 +0100

    edit accounts/urls.py file => 'connect the view chef_info and give it url using chef slug'

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('chefs/', views.chefs, name='chefs_accounts'),
+    path('chefs/<slug:slug>/', views.chef_info, name='chef_info'),
 ]
