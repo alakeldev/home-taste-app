ebdce6f38d1a5ba8e0917ce79b1a87be98b0323e
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-02-23 15:20:04 +0100

    connect login inside forms.py with views.py + create login.html file

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import Login
 
 # Create your views here.
 
@@ -19,4 +20,10 @@ def chef_info(request, slug):
 
     return render(request, 'account/chef_info.html', {
     'chef_info': chef_info,
+    })
+
+
+def user_login(request):
+
+    return render(request, 'account/login.html', {
     })
