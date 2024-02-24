3cb574f56eec40cc35c8bb8e74fd2d2d9049d2e4
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-02-24 18:12:51 +0100

    delete accounts application and all its files + static-group2/style.css

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'accounts'
-urlpatterns = [
-    path('chefs/', views.chefs, name='chefs'),
-    path('chefs/login/', views.user_login, name='login'),
-    path('chefs/<slug:slug>/', views.chef_info, name='chef_info'),
-]
