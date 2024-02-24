e3b68ca36e82d751c73ca23650e1a5d0e856dab2
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-02-24 12:03:16 +0100

    edit & tidy in base.html 'edit title & href for chefs link in nav' + edit sttings.py 'Apps comment' + edit accounts/urls.py 'another name to use it on the template'

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('chefs/', views.chefs, name='chefs_accounts'),
+    path('chefs/', views.chefs, name='chefs'),
     path('chefs/login/', views.user_login, name='login'),
     path('chefs/<slug:slug>/', views.chef_info, name='chef_info'),
 ]
