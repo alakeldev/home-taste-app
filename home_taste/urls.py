017b414aae93fff46b233aea196ce3d95ff58038
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-02-24 18:37:31 +0100

    create new file inside home app urls.py'call Index view in the file' + edit urls.py file project and connect it with urls file of home app

--- home_taste/urls.py
+++ home_taste/urls.py
@@ -16,15 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('accounts.urls', namespace='accounts')),
-    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG is True:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
