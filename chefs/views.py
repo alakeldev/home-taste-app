76f02998e624265f19ae1c017ad7ed21b0eb071e
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-02-25 20:34:14 +0100

    craete view chef_info method inside views.py 'to get slug from Profile model'

--- chefs/views.py
+++ chefs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import Profile
 # Create your views here.
 
 
@@ -7,4 +8,12 @@ def chefs_list(request):
     chefs = User.objects.all()
     return render(request, 'chefs.html', {
         'chefs' : chefs,
+    })
+
+
+def chef_info(request, slug):
+    chef_info = Profile.objects.get(slug=slug)
+
+    return render(request, 'chef_info.html', {
+        'chef_info' : chef_info
     })
