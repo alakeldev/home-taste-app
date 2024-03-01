2908dc14b265d6d6968f55fa50cca35ffda64d7a
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-03-01 03:11:43 +0100

    edit index.html 'add id to button' + edit main.js file 'add event listener onclick that leads to chefs page'

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,7 @@
 document.getElementById("mySignupButton").addEventListener("click", function () {
     window.open("/accounts/signup/", "_blank");
 });
+
+document.getElementById("myChefsButton").addEventListener("click", function () {
+    window.open("/chefs/", "_blank");
+});
