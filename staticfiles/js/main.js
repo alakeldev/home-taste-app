d3dd1a69777a763ad8c82abf1ec7699c554c02f7
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-03-04 19:22:25 +0100

    update inside staticfiles 'base.css' + 'main.js' after command collectstatic

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,3 +1,19 @@
+// Signup button on landing area home page
 document.getElementById("mySignupButton").addEventListener("click", function () {
     window.open("/accounts/signup/", "_blank");
 });
+
+// view chefs button on home page inside about section
+document.getElementById("myChefsButton").addEventListener("click", function () {
+    window.open("/chefs/", "_blank");
+});
+
+// related with the delete profile button inside the chef profile 'my_profile.html'
+function redirectToDeletePage() {
+    window.location.href = '/chefs/deleteprofile/';
+}
+
+// related with the update profile button inside the chef profile 'my_profile.html'
+function redirectToUpdatePage() {
+    window.location.href = '/chefs/editprofile/'; 
+}
