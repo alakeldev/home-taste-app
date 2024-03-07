937fb4559b7c3d577b693cff539ad3c76157127f
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-03-07 12:53:23 +0100

    edit settings.py 'allowed hosts = *'

--- home_taste/settings.py
+++ home_taste/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
