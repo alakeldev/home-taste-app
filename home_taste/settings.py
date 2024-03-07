43d2ce86cc05d8f4b01ded2c1ef0361cdd8851cb
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-03-07 13:12:19 +0100

    edit settings.py 'add port inside settings.py'

--- home_taste/settings.py
+++ home_taste/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+PORT = os.environ.get("PORT")
 ALLOWED_HOSTS = ['*']
 
 
