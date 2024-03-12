e3ac46a2cf2b16da79341e988a2399a6a14dd636
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-03-12 06:19:47 +0100

    edit chefs/views.py 'docstring added'

--- chefs/views.py
+++ chefs/views.py
@@ -9,7 +9,15 @@ from .forms import CommentForm
 
 # Create your views here.
 
+
 def chefs_list(request):
+    """
+    Retrieves a list of chefs cards based on search criteria.
+    If chefs are found, renders the 'chefs.html' template with 
+    the context = chefs cards.
+    Otherwise, displays a message indicating no match.
+
+    """
     search_query = request.GET.get('search', '')
     queryset = User.objects.all()
     if search_query:
@@ -26,6 +34,19 @@ def chefs_list(request):
 
 
 def chef_info(request, slug):
+    """
+    Retrieves information about a chef based on the provided slug.
+    If the request method is POST:
+        - Validates and saves a new comment related to the chef.
+        - Displays a success message if the comment is submitted successfully.
+        - Redirects back to the same page.
+        - If the comment submission fails, displays an error message.
+    If the request method is not POST:
+        - Initializes an empty comment form.
+        - Retrieves the chef's profile information and approved comments.
+        - Renders the 'chef_info.html' template with relevant context.
+    """
+
     chef_info = get_object_or_404(Profile, slug=slug)
     comments = Comment.objects.filter(chef=chef_info, is_approved=True)
     if request.method == 'POST':
@@ -50,6 +71,11 @@ def chef_info(request, slug):
 
 @login_required(login_url='/accounts/login/')
 def my_profile(request):
+    """
+    Renders the user's profile page if authenticated,
+    Otherwise redirects to login page
+    """
+
     return render(request, 'my_profile.html', {
         'my_profile' : my_profile
     })
@@ -57,6 +83,18 @@ def my_profile(request):
 
 @login_required(login_url='/accounts/login/')
 def edit_profile(request):
+    """
+    Allows users to edit their profile information.
+    If the request method is POST:
+        - Validates and saves the updated user profile.
+        - Displays a success message if the profile is updated successfully.
+        - Redirects to the 'my_profile' page.
+        - If the profile update fails, displays an error message.
+    If the request method is not POST:
+        - Initializes a form with the existing user profile data.
+        - Renders the 'edit_profile.html' template with the form.
+    """
+
     profile, created = Profile.objects.get_or_create(user=request.user)
     if request.method == 'POST':
         form = UserProfileForm(request.POST, request.FILES, instance=profile)
@@ -74,6 +112,16 @@ def edit_profile(request):
 
 @login_required(login_url='/accounts/login/')
 def delete_profile(request):
+    """
+    Deletes the user's profile upon receiving a POST request.
+    If the request method is POST:
+        - Deletes the user account associated with the request.
+        - Displays a success message indicating successful deletion.
+        - Redirects to the 'home' page.
+    If the request method is not POST:
+        - Renders the 'delete_profile.html' template.
+    """
+
     if request.method == 'POST':
         user = request.user
         user.delete()
