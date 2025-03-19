39f1847db119dec595700d355c6ef0c8d78cee3a
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2025-03-19 18:04:05 +0100

    edit settings.py change the smtp from outlook to gmail

--- home_taste/settings.py
+++ home_taste/settings.py
@@ -167,12 +167,13 @@ LOGIN_REDIRECT_URL = "/chefs/myprofile/"
 
 # Email configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp-mail.outlook.com"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "default_email@example.com")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "default_password")
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "webmaster@example.com")
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 # Static files configuration
 STATIC_URL = "/static/"
